58823e9470d3499fae929f49f7aae4947af94216
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2021-06-07 19:21:58 -0400

    Removidas pruebas para HashTables (podemos retornar con git reset)

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <string>
 
-#include "HashTable.h"
 #include "BinaryTree.h"
 #include "Node.h"
 using namespace std;
@@ -27,24 +26,6 @@ int main()
     tree.printInorder();
     cout << endl;
 
-    HashTable hash;
-
-    hash.AddItem("Samuel", "Coca cola");
-    hash.AddItem("Emily", "Juguito");
-    hash.AddItem("Juan", "Sprite");
-    hash.AddItem("Felipe", "Seven Up");
-    hash.AddItem("Carlos", "Romo");
-    hash.AddItem("Sabrina", "Gatdrink");
-    hash.AddItem("Salina", "Chuchita");
-    hash.AddItem("Corto", "Zapatero");
-
-    hash.RemoveItem("Corto");
-
-    cout << endl
-         << hash.GetValue("Samuel");
-    cout << endl
-         << hash.GetValue("Felipe");
-    cout << endl
-         << hash.GetValue("Corto");
+    
     return 0;
 }
